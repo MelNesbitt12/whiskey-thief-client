25b2254a372cc66d46f0f07aed1edf0ce10ce396
MelNesbitt12 <mel.nalband@gmail.com>
2020-10-06 21:00:16 -0400

    Moved whiskey update form into handlebars file
    moved inline styling into CSS file
    switched to arrow functions
    first pass at making app mobile responsive
    labeled modals in html file
    updated modal close for updateWhiskeySuccess function

--- assets/scripts/whiskeys/api.js
+++ assets/scripts/whiskeys/api.js
@@ -3,7 +3,7 @@
 const config = require('../config')
 const store = require('../store')
 
-const createWhiskey = function (formData) {
+const createWhiskey = (formData) => {
   return $.ajax({
     headers: {
       Authorization: 'Bearer ' + store.user.token
@@ -14,7 +14,7 @@ const createWhiskey = function (formData) {
   })
 }
 
-const indexWhiskey = function () {
+const indexWhiskey = () => {
   return $.ajax({
     headers: {
       Authorization: 'Bearer ' + store.user.token
@@ -27,19 +27,19 @@ const indexWhiskey = function () {
   })
 }
 
-const updateWhiskey = function (formData) {
-  const newWhiskeyId = formData.whiskey.id
+const updateWhiskey = (formData, whiskeyId) => {
+  // const newWhiskeyId = formData.whiskey.id
   return $.ajax({
     headers: {
       Authorization: 'Bearer ' + store.user.token
     },
-    url: config.apiUrl + '/whiskeys/' + newWhiskeyId,
+    url: config.apiUrl + '/whiskeys/' + whiskeyId,
     method: 'PATCH',
     data: formData
   })
 }
 
-const deleteWhiskey = function (whiskeyId) {
+const deleteWhiskey = (whiskeyId) => {
   return $.ajax({
     headers: {
       Authorization: 'Bearer ' + store.user.token
--- assets/scripts/whiskeys/ui.js
+++ assets/scripts/whiskeys/ui.js
@@ -3,22 +3,22 @@ const store = require('../store')
 const showWhiskeyTemplate = require('../templates/whiskey-listing2.handlebars')
 
 // if createWhiskey ajax request is successful, load success message, empty #all-whiskeys div to ensure that index is reset
-const createWhiskeySuccess = function () {
+const createWhiskeySuccess = () => {
   $('#myModalThree').modal('show')
   $('#all-whiskeys').hide()
   $('#all-whiskeys').empty()
-  $('.update-wrapper').hide()
+  // $('.update-wrapper').hide()
   $('form').trigger('reset')
 }
 
 // if createWhiskey ajax request fails, load failure message
-const createWhiskeyFailure = function () {
+const createWhiskeyFailure = () => {
   $('#message').text('Failed to add whiskey to your shelf.')
   $('#myModalThree').modal('hide')
 }
 
 // if indexWhiskey ajax request is sucessful, store all whiskeys and use handlebars template to display each whiskey as separate object on the page
-const indexWhiskeySuccess = function (response) {
+const indexWhiskeySuccess = (response) => {
   store.whiskey = response.whiskey
   const showWhiskeyHTML = showWhiskeyTemplate({ whiskeys: response.whiskeys })
   if (response.whiskeys.length === 0) {
@@ -29,43 +29,48 @@ const indexWhiskeySuccess = function (response) {
     $('#all-whiskeys').empty()
     $('#all-whiskeys').show()
     $('#all-whiskeys').append(showWhiskeyHTML)
-    $('.update-wrapper').show()
+    // $('.update-wrapper').show()
   }
   $('#see-whiskeys').hide()
 }
 
 // if indexWhiskey ajax request fails, load failure message
-const indexWhiskeyFailure = function () {
+const indexWhiskeyFailure = () => {
   $('#message').text('Could not get your whiskey shelf')
 }
 
 // if updateWhiskey ajax request is successful, reset #all-whiskeys div so that upon clicking #see-whiskeys button, updated index is displayed
-const updateWhiskeySuccess = function (response) {
+const updateWhiskeySuccess = (whiskeyId) => {
   $('#update-button').modal('hide')
-  $('.update-wrapper').hide()
+  // $('.update-wrapper').hide()
   $('#myModalSeven').modal('show')
-  $('#myModalFour').modal('hide')
-  $('form').trigger('reset')
-  $('#all-whiskeys').empty()
-  $('#see-whiskeys').show()
+  $(`#myModalFour-${whiskeyId}`).modal('hide')
+  $('.modal-backdrop').removeClass('show')
+  return new Promise(resolve => setTimeout(() => {
+    $('.modal-backdrop').remove()
+    resolve()
+  }, 0))
 }
+$('form').trigger('reset')
+$('#all-whiskeys').empty()
+$('#see-whiskeys').show()
 
 // if updateWhiskey ajax request fails, show failure message
-const updateWhiskeyFailure = function () {
+const updateWhiskeyFailure = (whiskeyId) => {
   $('#message').text('Could not update your whiskey')
-  $('#myModalFour').modal('hide')
+  $(`#myModalFour-${whiskeyId}`).modal('hide')
 }
 
 // if deleteWhiskey ajax request is successful, reset #all-whiskeys div so that upon clicking #see-whiskeys button, updated index is displayed
-const deleteWhiskeySuccess = function () {
+const deleteWhiskeySuccess = () => {
   $('#myModalFive').modal('show')
   $('#all-whiskeys').hide()
   $('#see-whiskeys').show()
-  $('.update-wrapper').hide()
+  // $('.update-wrapper').hide()
 }
 
 // if deleteWhiskey ajax request fails, show failure message
-const deleteWhiskeyFailure = function () {
+const deleteWhiskeyFailure = () => {
   $('#message').text('Could not delete whiskey')
   $('#myModalFive').modal('hide')
 }
